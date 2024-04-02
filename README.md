ec0c9921eb9955f21b263dbf164d3a772693a949
mlopezd <44083524+mlopezd@users.noreply.github.com>
2024-04-02 11:14:02 +1100

    Add all files

---
+++ README.md
@@ -0,0 +1,11 @@
+## Installation
+
+Just run:
+
+    docker compose up
+
+ It may take a few minutes to start all the containers, once it does, AWX web interface will be available at http://localhost/ using the following credentials:
+
+**Username:** admin 
+
+**Password:** JlA1BkrquJoF2QxRT6rc
---
+++ credentials.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "awx",
+        'USER': "awx",
+        'PASSWORD': "IpIrpQ2l4W",
+        'HOST': "postgres",
+        'PORT': "5432",
+    }
+}
+
+BROADCAST_WEBSOCKET_SECRET = "bllJS1lLcVBNbywwZjl3aUFKeDVZMHdLNnluRl86eFJLLkRpSUxLNDdIR3pYZDJiZF9VaWZDLGR5VHNKMi1KUEQ1VzdNUU5TLWFUNS16QkkzZzZpUk9tMWtyWHJxLmdVMTJ1dDhoTm8zTVpNZUZSTFkzZWM3a3I5YTNMZUhnMkU="
